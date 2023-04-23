5f318da577ff7721af26ee43a2c4697bf2c9628c
Ethan Christensen <ethanlchristensen@outlook.com>
2023-04-23 02:56:56 -0500

    erm

--- config.h
+++ config.h
@@ -13,11 +13,7 @@ constexpr int DIRECTIONS[][2] = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}, {1, 1}, {1, -
 constexpr int ALIVE = COLOR_SIZE-1;
 constexpr int BORN = int(8.0 / 10.0 * ALIVE);
 constexpr bool EXPLOSION = false;
-constexpr bool SPANT = true;
+constexpr bool SPANT = false;
 
-/* 
-This include is here because interpolations.h is dependent on the above variables.
-I am not a C++ dev.
-*/
 #include "interpolations.h"
 constexpr auto INTERPOLATION = solarized;
